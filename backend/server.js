0d1db8a10aaa84aa926bd3d2009a82303c7ae897
Pablo Rochera <pablorocheragarcia@gmail.com>
2025-06-02 16:30:03 +0200

    url modificada frontend

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/", limiter)
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "my-fit-journey-6wltpqadh-pablorocheras-projects.vercel.app",
     credentials: true,
   })
 )
